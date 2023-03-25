6f5a6326481f2d5db3971b83a0c8a10784f8d83a
Zara Martín <zmartand@myuax.es>
2023-03-25 14:17:40 +0100

    Clase ClaseCadena para usuario

--- src/main/java/ClaseCadena.java
+++ src/main/java/ClaseCadena.java
@@ -1,29 +1,47 @@
 import java.util.Scanner;
 
+
 public class ClaseCadena {
     private String nombre;
+    private String iniciales;
     private String apellido;
+    public static String DevuelveIniciales(String nombre, String apellido) {
 
-    public ClaseCadena(String nombre, String apellido) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
-    public String DevuelveIniciales(){
-        String[] nombreSeparado = nombre.split(" ");
-        String[] apellidoSeparado = apellido.split(" ");
-        String iniciales = "";
-        for (int i = 0; i < nombreSeparado.length; i++) {
-            iniciales += nombreSeparado[i].charAt(0) + ".";
+        if(nombre == null || nombre.isEmpty() || apellido == null || apellido.isEmpty()) {
+            return "";
         }
-        for (int i = 0; i < apellidoSeparado.length; i++) {
-            iniciales += apellidoSeparado[i].charAt(0) + ".";
-        }
-        return iniciales;
-    }
-    public static void main(String[] args) {
-        ClaseCadena cc = new ClaseCadena("Andreas", "Garcia");
-        System.out.println(cc.DevuelveIniciales());
-    }
 
+        String[] nombres = nombre.split(" ");
+        String[] apellidos = apellido.split(" ");
+        StringBuilder iniciales = new StringBuilder();
+
+        for(String nombreActual : nombres) {
+            if(!nombreActual.isEmpty()) {
+                iniciales.append(nombreActual.charAt(0)).append(".");
+            }
+        }
+        for(String apellidoActual : apellidos) {
+            if(!apellidoActual.isEmpty()) {
+                iniciales.append(apellidoActual.charAt(0)).append(".");
+            }
+        }
+
+        return iniciales.toString().toUpperCase();
+    }
+    public void Menu() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese su nombre: ");
+        nombre = sc.nextLine();
+        System.out.print("Ingrese su apellido: ");
+        apellido = sc.nextLine();
+        iniciales = DevuelveIniciales(nombre, apellido);
+
+        if(iniciales.isEmpty()) {
+            System.out.println("No se ingresó un nombre completo válido.");
+        } else {
+            System.out.println("Las iniciales de su nombre completo son: " + iniciales);
+        }
+
+    }
 }
-
