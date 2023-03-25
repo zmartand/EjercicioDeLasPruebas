dcf98557d6bc5ff3d04761d4aeeb9753d6d1f555
Zara Martín <zmartand@myuax.es>
2023-03-25 14:32:06 +0100

    Ejercicio 1 corrección

--- src/main/java/ClaseCadena.java
+++ src/main/java/ClaseCadena.java
@@ -1,46 +1,21 @@
-import java.util.Scanner;
 public class ClaseCadena {
-    private String nombre;
-    private String iniciales;
-    private String apellido;
 
-    public static String DevuelveIniciales(String nombre, String apellido) {
+    public static String DevuelveIniciales(String nombreCompleto) {
 
-        if(nombre == null || nombre.isEmpty() || apellido == null || apellido.isEmpty()) {
+        if(nombreCompleto == null || nombreCompleto.isEmpty()) {
             return "";
         }
 
-        String[] nombres = nombre.split(" ");
-        String[] apellidos = apellido.split(" ");
+        String[] nombres = nombreCompleto.split(" ");
         StringBuilder iniciales = new StringBuilder();
 
-        for(String nombreActual : nombres) {
-            if(!nombreActual.isEmpty()) {
-                iniciales.append(nombreActual.charAt(0)).append(".");
-            }
-        }
-        for(String apellidoActual : apellidos) {
-            if(!apellidoActual.isEmpty()) {
-                iniciales.append(apellidoActual.charAt(0)).append(".");
+        for(String nombre : nombres) {
+            if(!nombre.isEmpty()) {
+                iniciales.append(nombre.charAt(0)).append(".");
             }
         }
 
         return iniciales.toString().toUpperCase();
     }
-    public void Menu() {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Ingrese su nombre: ");
-        nombre = sc.nextLine();
-        System.out.print("Ingrese su apellido: ");
-        apellido = sc.nextLine();
-        iniciales = DevuelveIniciales(nombre, apellido);
-
-        if(iniciales.isEmpty()) {
-            System.out.println(" ");
-        } else {
-            System.out.println("Las iniciales de su nombre completo son: " + iniciales);
-        }
-
-    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         ClaseCadena cc = new ClaseCadena();
-        cc.Menu();
+        System.out.println(cc.DevuelveIniciales("Andreas Dulac"));
     }
 }
